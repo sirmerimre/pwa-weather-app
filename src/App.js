e371a03811b640758a7bc777394f642b03d8f163
Imre Sirmer <sirmer.imre@gmail.com>
2022-08-06 14:36:54 +0100

    Add install button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-import { fetchWeather } from './api/fetchWeather';
+import {fetchWeather} from './api/fetchWeather';
 import './App.css';
 
 const App = () => {
     const [query, setQuery] = useState('');
     const [weather, setWeather] = useState({});
+    let hideButton;
 
     const search = async (e) => {
-        if(e.key === 'Enter') {
+        if (e.key === 'Enter') {
             const data = await fetchWeather(query);
 
             setWeather(data);
@@ -16,9 +17,36 @@ const App = () => {
         }
     }
 
+    let deferredPrompt;
+    window.addEventListener('beforeinstallprompt', (e) => {
+        e.preventDefault();
+        deferredPrompt = e;
+    })
+
+    const onClickHandler = () => {
+        if (deferredPrompt !== null) {
+            deferredPrompt.prompt().then(() => {
+                deferredPrompt.userChoice.then((outcome) => {
+                    if (outcome === 'accepted') {
+                        deferredPrompt = null;
+                    }
+                })
+            });
+
+        }
+    }
+
+    if(window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone === true) {
+        hideButton = true
+    }
+
     return (
         <div className="main-container">
-            <input type="text" className="search" placeholder="Search..." value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search}/>
+            <div className={'button-container'} hidden={hideButton}>
+                <button id="install-app-button" onClick={onClickHandler}>Install App</button>
+            </div>
+            <input type="text" className="search" placeholder="Search..." value={query}
+                   onChange={(e) => setQuery(e.target.value)} onKeyPress={search}/>
             {weather.main && (
                 <div className="city">
                     <h2 className="city-name">
@@ -30,7 +58,9 @@ const App = () => {
                         <sup>&deg;C</sup>
                     </div>
                     <div className="info">
-                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                        <img className="city-icon"
+                             src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                             alt={weather.weather[0].description}/>
                         <p>{weather.weather[0].description}</p>
                     </div>
                 </div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import LocalServiceWorkerRegister from './sw-register';
 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>)
+LocalServiceWorkerRegister();
